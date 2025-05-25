f57bc2128a8706b5422b77b8d9f31db395bdcdd6
Kimjunho <39546306+xjvmdutl@users.noreply.github.com>
2025-05-25 16:23:11 +0900

    Auth (#5)
    - 읽기 전용 Repository 생성한다.
    - swagger 를 작성한다.
    - 공통 Response를 작성한다.
    - QueryDsl 설정한다.
    - Resource 설정한다
    
    Co-authored-by: junhokim <junhokim@gmarket.com>

---
+++ src/main/java/com/retrip/auth/infra/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.retrip.auth.infra.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .components(
+                        new Components()
+                ).info(
+                        new Info()
+                                .title("Auth Application")
+                                .version("v0.0.1")
+                );
+    }
+    @Bean
+    public GroupedOpenApi authApi(){
+        return GroupedOpenApi.builder()
+                .group("auth")
+                .pathsToMatch("/auth/**")
+                .build();
+    }
+}
