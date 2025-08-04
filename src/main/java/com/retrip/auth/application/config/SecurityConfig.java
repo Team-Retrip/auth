b542be4a361d7a1b06cf829646fbf28f6dc21c3f
junhokim <junhokim@gmarket.com>
2025-08-04 22:04:40 +0900

    Auth
    - CORS 추가

--- src/main/java/com/retrip/auth/application/config/SecurityConfig.java
+++ src/main/java/com/retrip/auth/application/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://3.39.117.218:8080"));
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
