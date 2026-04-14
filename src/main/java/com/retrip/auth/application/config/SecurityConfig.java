d55f6c1184633554808cf41c0d7635a23fe5b9ed
p_caso <ekfrehd@github.com>
2026-04-14 18:26:54 +0900

    vercel.web 모두 허용

--- src/main/java/com/retrip/auth/application/config/SecurityConfig.java
+++ src/main/java/com/retrip/auth/application/config/SecurityConfig.java
@@ -136,6 +136,7 @@ public class SecurityConfig {
                 "http://127.0.0.1:*",
                 "https://retrip-web.vercel.app",
                 "https://retrip-web-*.vercel.app",
+                "https://*-*.vercel.app",
                 "https://retrip.io",
                 "https://*.retrip.io"
         ));
